161abc7a80d8db6f3a4ac85a5238ec95f20962a7
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-10-05 14:31:37 +0200

    Add initial AKS resources. - 7

--- output.tf
+++ output.tf
@@ -1,19 +1,23 @@
 output "resource_group_name" {
-  value = azure_aks.allianz-demo-aks-rg.name
+  value = azurerm_resource_group.allianz-demo-aks-rg.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.allianz-demo-aks-rg.location
 }
 
 output "acr_name" {
-  value = azure_aks.allianz-demo-acr.name
+  value = azurerm_container_registry.allianz-demo-acr.name
 }
 
 output "aks_name" {
-  value = azure_aks.allianz-demo-aks.name
+  value = azurerm_kubernetes_cluster.allianz-demo-aks.name
 }
 
 output "aks_ra_principal_id" {
-  value = azure_aks.allianz-demo-aks-ra.principal_id
+  value = azurerm_role_assignment.allianz-demo-aks-ra.principal_id
 }
 
 output "aks_nodepool_name" {
-  value = azure_aks.allianz-demo-nodepool.name
+  value = azurerm_kubernetes_cluster_node_pool.allianz-demo-nodepool.name
 }
