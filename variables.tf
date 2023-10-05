d0e9ce568f7cca0cf502e4fd402956c6116c5f24
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-10-05 14:07:25 +0200

    Add initial AKS resources. - 3

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "rg-name" {
+  description = "Name of Resource Group"
+  type        = string
+}
+
+variable "rg-location" {
+  description = "Location of Resource Group"
+  type        = string
+}
+
+variable "acr-name" {
+  description = "Name of ACR"
+  type        = string
+}
+
+variable "aks-name" {
+  description = "Name of AKS"
+  type        = string
+}
+
+variable "nodepool-name" {
+  description = "Name of Node Pool"
+  type        = string
+}
